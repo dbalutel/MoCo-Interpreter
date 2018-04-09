2e59be3d9435426e36d2273496c37c0ecdd7327c
dbalutsel <dmitrybalutsel@gmail.com>
2018-04-10 00:04:56 +0300

    Init

---
+++ src/test/java/md/balutsel/mocointerpreter/MocoInterpreterApplicationTests.java
@@ -0,0 +1,16 @@
+package md.balutsel.mocointerpreter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MocoInterpreterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
