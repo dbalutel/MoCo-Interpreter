6bca7512408ea6437381f1adeed484357cb5c7b8
dbalutsel <dmitrybalutsel@gmail.com>
2018-04-16 20:43:44 +0300

    Added dependencies and datasource config

--- src/main/java/md/balutsel/mocointerpreter/MocoInterpreterApplication.java
+++ src/main/java/md/balutsel/mocointerpreter/MocoInterpreterApplication.java
@@ -1,12 +1,26 @@
 package md.balutsel.mocointerpreter;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MocoInterpreterApplication {
-
+public class MocoInterpreterApplication implements CommandLineRunner {
+    
     public static void main(String[] args) {
         SpringApplication.run(MocoInterpreterApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) {
+    }
+
+    @Bean
+    protected Module module() {
+        return new Hibernate5Module();
+    }
 }
+
