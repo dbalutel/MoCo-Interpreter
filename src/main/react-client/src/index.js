9a244132a86d7c220a70e9a3e3bacfb9b61022be
dbalutsel <dmitrybalutsel@gmail.com>
2018-04-18 17:04:54 +0300

    Barebone course page

--- src/main/react-client/src/index.js
+++ src/main/react-client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CourseView from './components/CourseView';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CourseView />, document.getElementById('root'));
 registerServiceWorker();
